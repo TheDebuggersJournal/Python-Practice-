2fa22525074b36dba3d2411f5efc88ad7e1a4846
TheDebuggersJournal <pratik45walunj@gmail.com>
2024-12-27 13:23:04 +0530

    Create Problem2.py

---
+++ Basic Python/Problem2.py
@@ -0,0 +1,50 @@
+def arithmatic_operations(integer1,integer2):
+    print(''' List of Available operations:
+          1. Addition
+          2. Subtraction
+          3. Multiplication
+          4. Division
+          5. Floor Division
+          6. Modulus
+          7. Exponential
+          8. Exit
+''')
+    while True:
+        choice = int(input("Enter your choice: "))
+        try:
+
+            if choice == 1:
+                result = integer1+integer2
+                print(result)
+            elif choice == 2:
+                result = integer1-integer2
+                print(result)
+            elif choice == 3:
+                result = integer1 * integer2
+                print(result)
+            elif choice == 4:
+                result = integer1/integer2
+                print(result)
+            elif choice == 5:
+                result = integer1//integer2
+                print(result)
+            elif choice == 6:
+                result = integer1 % integer2
+                print(result)
+            elif choice == 7:
+                result = integer1 ** integer2
+                print(result)
+            elif choice == 8:
+                print("Thankyou for using!!!")
+                break
+            else:
+                print("Invalid Input")
+            
+        except ZeroDivisionError:
+            print("Number cannot be divisible by zero")        
+
+
+
+integer1 = float(input("Enter first integer: "))
+integer2  = float(input("Enter second integer: "))
+print(arithmatic_operations(integer1,integer2))
